5de73c5c3ca6188d01899032165a2041d2e06a1c
James Boland <102449868+JRBoland@users.noreply.github.com>
2023-07-19 14:00:58 +1000

    added getAllBookingsForUser in BookingFunctions, added functionality for getBookingById

--- src/controllers/BookingFunctions.js
+++ src/controllers/BookingFunctions.js
@@ -2,12 +2,33 @@ const { Booking } = require('../models/BookingModel');
 const { User } = require('../models/UserModel');
 const { Van } = require('../models/VanModel');
 
+// Retrieve all bookings, admin only
 async function getAllBookings() {
   return await Booking.find({}).exec();
 }
 
-async function getBookingById(bookingID) {
-  return await Booking.findById(bookingID).exec();
+// Retrieve all bookings pertaining to user
+async function getAllBookingsForUser(userID) {
+  return await Booking.find({ user: userID }).exec();
+}
+
+// Retrieve booking by ID, user can only retrieve own booking by ID.
+async function getBookingById(bookingID, userID) {
+  if (userID) {
+    // Check if the user is an admin
+    const user = await User.findById(userID);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    if (user.admin) {
+      return await Booking.findById(bookingID).exec();
+    } else {
+      return await Booking.findOne({ _id: bookingID, user: userID }).exec();
+    }
+  } else {
+    // If userID is not provided, just get the booking without user check
+    return await Booking.findById(bookingID).exec();
+  }
 }
 
 async function createBooking(userID, vanID, startDate, endDate, totalPrice) {
@@ -34,6 +55,7 @@ async function deleteBooking(bookingID) {
 
 module.exports = {
   getAllBookings,
+  getAllBookingsForUser,
   getBookingById,
   createBooking,
   updateBooking,
